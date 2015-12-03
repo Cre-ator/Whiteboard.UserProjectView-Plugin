2aeaa7dd0daa4a38d4539a3d186aac6a48a07798
Stefan Schwarz <s.schwarz@online.de>
2015-12-03 15:18:26 +0100

    Some changes

--- UserProjectView/core/UPDatabase_api.php
+++ UserProjectView/core/UPDatabase_api.php
@@ -18,9 +18,21 @@ class UPDatabase_api
       $this->mysqli = new mysqli( $this->dbPath, $this->dbUser, $this->dbPass, $this->dbName );
    }
 
+   public function getMantisVersion()
+   {
+      return substr( MANTIS_VERSION, 0, 4 );
+   }
+
    public function resetPlugin()
    {
-      $config_table = db_get_table( 'mantis_config_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $config_table = db_get_table( 'mantis_config_table' );
+      }
+      else
+      {
+         $config_table = db_get_table( 'config' );
+      }
 
       $query = "DELETE FROM $config_table
           WHERE config_id LIKE 'plugin_UserProjectView_%'";
@@ -30,7 +42,14 @@ class UPDatabase_api
 
    public function getAllUsers()
    {
-      $user_table = db_get_table( 'mantis_user_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $user_table = db_get_table( 'mantis_user_table' );
+      }
+      else
+      {
+         $user_table = db_get_table( 'user' );
+      }
 
       $query = "SELECT u.id FROM $user_table u
           WHERE u.access_level < " . config_get_global( 'admin_site_threshold' );
@@ -42,7 +61,14 @@ class UPDatabase_api
 
    public function getProjectV( $version )
    {
-      $project_version_table = db_get_table( 'mantis_project_version_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $project_version_table = db_get_table( 'mantis_project_version_table' );
+      }
+      else
+      {
+         $project_version_table = db_get_table( 'project_version' );
+      }
 
       $query = "SELECT v.project_id FROM $project_version_table v
           WHERE v.version = '" . $version . "'";
@@ -55,7 +81,14 @@ class UPDatabase_api
 
    public function getIssuesUPTS( $user_id, $project_id, $target_version, $status )
    {
-      $bug_table = db_get_table( 'mantis_bug_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $bug_table = db_get_table( 'mantis_bug_table' );
+      }
+      else
+      {
+         $bug_table = db_get_table( 'bug' );
+      }
 
       $query = "SELECT b.id FROM $bug_table b
           WHERE b.handler_id = " . $user_id . "
@@ -73,7 +106,14 @@ class UPDatabase_api
 
    public function getAmountOfIssuesUPTS( $user_id, $project_id, $target_version, $status )
    {
-      $bug_table = db_get_table( 'mantis_bug_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $bug_table = db_get_table( 'mantis_bug_table' );
+      }
+      else
+      {
+         $bug_table = db_get_table( 'bug' );
+      }
 
       $query = "SELECT COUNT(*) FROM $bug_table b
           WHERE b.handler_id = " . $user_id . "
@@ -92,7 +132,14 @@ class UPDatabase_api
 
    public function getAmountOfIssuesUPS( $user_id, $project_id, $status )
    {
-      $bug_table = db_get_table( 'mantis_bug_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $bug_table = db_get_table( 'mantis_bug_table' );
+      }
+      else
+      {
+         $bug_table = db_get_table( 'bug' );
+      }
 
       $query = "SELECT COUNT(*) FROM $bug_table b
           WHERE b.handler_id = " . $user_id . "
@@ -110,7 +157,14 @@ class UPDatabase_api
 
    public function checkUserIsAssignedToProject( $user_id, $project_id )
    {
-      $project_user_list_table = db_get_table( 'mantis_project_user_list_table' );
+      if ( $this->getMantisVersion() == '1.2.' )
+      {
+         $project_user_list_table = db_get_table( 'mantis_project_user_list_table' );
+      }
+      else
+      {
+         $project_user_list_table = db_get_table( 'project_user_list' );
+      }
 
       $query = "SELECT p.user_id FROM $project_user_list_table p
           WHERE p.project_id = " . $project_id . "
